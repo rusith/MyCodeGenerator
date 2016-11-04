61e200b314a6a312595dff48d0948671eae1926c
rusith <rusith@bourketechnologies.com>
2016-11-04 09:45:41 +0530

    Some changes

---
+++ Generators/SpGenerator.cs
@@ -0,0 +1,13 @@
+﻿using DatabaseSchemaReader.DataSchema;
+using MyCodeGenerator.Models;
+
+namespace MyCodeGenerator.Generators
+{
+    public class SpGenerator
+    {
+        public static Sp Generate(DatabaseStoredProcedure procedure)
+        {
+            return new Sp { Name = procedure.Name, Content = TemplateGenarator.GenerateStoredProcedure(procedure),StoredProcedure = procedure};
+        }
+    }
+}
---
+++ Models/Sp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseSchemaReader.DataSchema;
+
+namespace MyCodeGenerator.Models
+{
+    public class Sp
+    {
+        public string Content { get; set; }
+        public string Name { get; set; }
+        public DatabaseStoredProcedure StoredProcedure { get; set; }
+    }
+}

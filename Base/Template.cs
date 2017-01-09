2d3d439a397aed51b54853fffe3ea51195fcfa04
rusith <srlsooriyagoda@gmail.com>
2017-01-09 07:54:09 +0530

    new Classes

---
+++ Base/Template.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace MyCodeGenerator.Base
+{
+    public class Template
+    {
+        private StringBuilder _builder;
+
+        public Template(StringBuilder str)
+        {
+            _builder = str;
+        }
+
+        /// <summary>
+        /// Set a variable value of the template
+        /// </summary>
+        /// <param name="name">value name to set</param>
+        /// <param name="replacement">value to set</param>
+        /// <returns></returns>
+        public Template Set(string name, string replacement)
+        {
+            _builder.Replace("$" + name + "$", replacement);
+            return this;
+        }
+
+        public string Build()
+        {
+            return _builder.ToString();
+        }
+    }
+}
---
+++ Base/Templates.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MyCodeGenerator.Base
+{
+    public class Templates
+    {
+        private static Templates _object;
+        private Dictionary<string, Template>  _templates;
+             
+        private Templates()
+        {
+            ReadTemplate();   
+        }
+
+        public static Templates All => _object ?? (_object = new Templates());
+
+        private void ReadTemplate()
+        {
+            const string templateFile = "\\Templates\\template";
+            if(!File.Exists(templateFile))
+                return;
+            var lines = File.ReadLines(templateFile).ToList();
+            if(lines.Count<1)
+                return;
+
+            _templates = new Dictionary<string, Template>();
+
+            for (var i = 0; i < lines.Count; i++)
+            {
+                var line = lines[i];
+                string currentTemplateName;
+                if (!line.StartsWith("##") || line.Length <= 2 ||
+                    string.IsNullOrWhiteSpace(currentTemplateName = line.Substring(2, line.Length - 1))) continue;
+
+                var currentTemplateBuilder = new StringBuilder();
+                while (line == "###" || i>=(lines.Count-1))
+                {
+                    currentTemplateBuilder.AppendLine(line);
+                    i++;
+                }
+                _templates.Add(currentTemplateName,new Template(currentTemplateBuilder));
+            }
+        }
+
+        /// <summary>
+        /// Get the template with the given name 
+        /// </summary>
+        /// <param name="name">name of the template </param>
+        /// <returns></returns>
+        public Template this[string name] => _templates[name];
+    }
+}
